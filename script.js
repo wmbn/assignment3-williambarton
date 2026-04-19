3954c7e5216f558ac8f4a753b6072cd7d4117408
wmbn <114208296+wmbn@users.noreply.github.com>
2026-04-19 14:33:24 -0400

    Update script.js with comments
    Added comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+//theme toggle button and body element
 const btn = document.getElementById('themeToggle');
 const body = document.body;
 
+//localStorage for saved theme
 if (localStorage.getItem('dark') === 'on') {
     body.classList.add('dark');
 }
 
+//add click event listener to button
 btn.addEventListener('click', () => {
     body.classList.toggle('dark');
     localStorage.setItem('dark', body.classList.contains('dark') ? 'on' : 'off');
